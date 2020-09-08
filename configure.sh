7af3cb7ebebb91c875ab9356f3338cd8155ac7e3
Pormes <66345108+pormes@users.noreply.github.com>
2020-09-08 11:19:26 +0700

    Update configure.sh

--- configure.sh
+++ configure.sh
@@ -15,11 +15,14 @@ pkg install pkg-config -y
 pkg install automake -y
 pkg install asciidoc -y
 aclocal -I m4 && autoheader && autoconf && automake --add-missing --copy
-sh autogen.sh && ./configure && make && make install
+sh autogen.sh && ./configure && make
 
 echo "DONE ... !!!"
 echo "PLEASE SUPPORT pormes17 ON YOUTUBE"
 echo "coded by pormes"
+echo ""
+echo ""
+echo ""
 echo "█▀█░█▀█░█▀█░█▀█░█░█░█▄█░█▀█░█░█░█▀▀"
 echo "█▀█░█░█░█░█░█░█░░█░░█░█░█░█░█░█░▀▀█"
 echo "▀░▀░▀░▀░▀▀▀░▀░▀░░▀░░▀░▀░▀▀▀░▀▀▀░▀▀▀ © 2020 GitHub, Inc."
